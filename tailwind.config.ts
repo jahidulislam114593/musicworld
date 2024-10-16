6f3288f58a879763f6105db825daa08c65f84267
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-10-16 22:05:43 +0600

    [NextJs, Aceternity UI]

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -44,6 +44,8 @@ function addVariablesForColors({ addBase, theme }: any) {
   });
 }
 
+
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -56,8 +58,17 @@ const config: Config = {
       animation:{
         scroll:"scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
         spotlight:"spotlight 2s ease .75s 1 forwards",
+        "meteor-effect": "meteor 5s linear infinite",
       },
       keyframes:{
+        meteor:{
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
+          },
+        },
         scroll:{
           to:{
             transform: "translate(calc(-50% - 0.5rem))",
